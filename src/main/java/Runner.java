12de5807886e5e345109c2c250f2bb469edb44de
Alexander Pashkevich <pashkevich.a.a@mail.ru>
2022-11-01 20:12:14 +0300

    first Commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+import tasks.FirstTask;
+
+public class Runner {
+    public static void main(String[] args) {
+        final FirstTask firstTask = new FirstTask();
+        firstTask.run();
+    }
+}
